486de81627b37c95b35c6184290ea0d4a1a2f59b
pandaloves <meiyinghu18@gmail.com>
2023-10-24 17:05:23 +0200

    new

---
+++ script.js
@@ -0,0 +1,42 @@
+const restaurantName = "Danli";
+var order = [];
+
+window.addEventListener("DOMContentLoaded", function () {
+  updateCart();
+
+  // Add the Cancel link event
+  const lnkCancel = document.querySelector("#lnkStartOver");
+  lnkCancel.addEventListener("click", function () {
+    const areYouSure = confirm("Är du säker på att avbryta beställningen?");
+    if (areYouSure) {
+      order = [];
+      updateCart();
+    }
+  });
+  // Add the meals click funciton
+  const meals = document.querySelectorAll(".meal");
+  for (let meal of meals) {
+    meal.addEventListener("click", function () {
+      let title = this.dataset.title;
+      order.push(title);
+      updateCart();
+    });
+  }
+});
+
+// Add the update cart function
+function updateCart() {
+  let html = "";
+  for (let meal of order) {
+    html += "<li>" + meal + "</li>";
+  }
+  const ul = document.querySelector("#cart ul");
+  ul.innerHTML = html;
+
+  const cart = document.querySelector("#cart");
+  if (order.length == 0) {
+    cart.style.backgroundColor = "#C8C8C8";
+  } else {
+    cart.style.backgroundColor = " rgb(40, 40, 172)";
+  }
+}
